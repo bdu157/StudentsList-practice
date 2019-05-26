20af1791f9edc5ef2f6a88e1c863140d627bb79e
Dongwoo Pae <dongwoopae@gmail.com>
2019-05-26 15:05:38 -0700

    set up mainviewcontroller

--- StudentsList(practice)/MainViewController.swift
+++ StudentsList(practice)/MainViewController.swift
@@ -11,11 +11,35 @@ import UIKit
 class MainViewController: UIViewController {
 
     @IBOutlet weak var sortbySegmentedControl: UISegmentedControl!
-    
     @IBOutlet weak var filterbySegmentedControl: UISegmentedControl!
+    
+    var studentController = StudentController()
+    var studentListTableViewController: StudentListTableViewController! {
+        didSet {
+            updateData()
+        }
+    }
+    
+    private var students: [Student] = [] {
+        didSet {
+            updateData()
+        }
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.studentController.loadFromPersistentStore { (students, error) in
+            if let error = error {
+                print("There was an error loading students \(error)")
+                return
+            } else {
+                DispatchQueue.main.async {
+                    self.students = students ?? []
+                }
+            }
+        }
+        
 
     }
     
@@ -27,11 +51,34 @@ class MainViewController: UIViewController {
 
     
     @IBAction func SortBy(_ sender: UISegmentedControl) {
-        
+        updateData()
     }
     
     @IBAction func FilterBy(_ sender: UISegmentedControl) {
-        
+        updateData()
     }
     
+    private func updateData() {
+        var filterAndSortedList : [Student]
+        
+        switch filterbySegmentedControl.selectedSegmentIndex {
+        case 1:
+            filterAndSortedList = self.students.filter {$0.course == "iOS"}
+        case 2:
+            filterAndSortedList = self.students.filter {$0.course == "Web"}
+        case 3:
+            filterAndSortedList = self.students.filter {$0.course == "UX"}
+        default:
+            filterAndSortedList = self.students
+        }
+        
+        if self.sortbySegmentedControl.selectedSegmentIndex == 0 {
+            filterAndSortedList = filterAndSortedList.sorted(by: { (student, nextStudent) -> Bool in
+                return student.firstName < nextStudent.firstName
+            })
+        } else {
+            filterAndSortedList = filterAndSortedList.sorted {$0.lastName < $1.lastName}
+        }
+        self.studentListTableViewController.students = filterAndSortedList
+    }
 }
